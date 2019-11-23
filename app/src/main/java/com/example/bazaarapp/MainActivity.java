3930b7f9735fdd2f804669b24b5e2b396476646c
FaiqaIftikhar <faiqaiftikharali@gmail.com>
2019-11-24 01:34:09 +0500

    faiqaSeven

--- app/src/main/java/com/example/bazaarapp/MainActivity.java
+++ app/src/main/java/com/example/bazaarapp/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.bazaarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button button;
     TextView textview;
+    FirebaseAuth mAuth;
 
+    EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        email=findViewById(R.id.editText3);
+        password=findViewById(R.id.editText4);
         addListenerOnTextView();
         addListenerOnButton();
 
@@ -35,8 +52,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
 
+
                 Intent intent = new Intent(context, activitySignup.class);
-              //  intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                //  intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(intent);
 
             }
@@ -58,8 +76,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
 
-                Intent intent = new Intent(context, home.class);
-                startActivity(intent);
+                String Email=email.getText().toString().trim();
+                String Password=password.getText().toString().trim();
+                if (TextUtils.isEmpty(Email)){
+                    Toast.makeText(context,"Please enter your Email Address to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }if (TextUtils.isEmpty(Password)){
+                    Toast.makeText(context,"Please enter your password to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    Toast.makeText(context,"Please enter a valid Email address!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            Intent intent = new Intent(getApplicationContext(), home.class);
+                            startActivity(intent);
+
+                        }
+                        else{
+                            Toast.makeText(context,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    }
+                });
+
+
 
             }
 
--- app/src/main/java/com/example/bazaarapp/activitySignup.java
+++ app/src/main/java/com/example/bazaarapp/activitySignup.java
@@ -1,24 +1,33 @@
 package com.example.bazaarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class activitySignup extends AppCompatActivity {
-EditText fName,lName,mail,passw;
+EditText fName,lName,mail,passw,confirm;
     Button button;
     DatabaseReference reff;
     DatabaseReference ref;
     User user;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +36,14 @@ EditText fName,lName,mail,passw;
         lName=findViewById(R.id.editTextlname);
         mail=findViewById(R.id.editTextemail);
         passw=findViewById(R.id.editTextpassword);
-        user=new User();
-        reff= FirebaseDatabase.getInstance().getReference().child("User");
+        confirm=findViewById(R.id.editText_reenterpassword);
 
-        ref= FirebaseDatabase.getInstance().getReference().child("User");
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        //user=new User();
+      //  reff= FirebaseDatabase.getInstance().getReference().child("User");
+
+        //ref= FirebaseDatabase.getInstance().getReference().child("User");
 
         addListenerOnButton();
 
@@ -49,16 +62,73 @@ EditText fName,lName,mail,passw;
 
             @Override
             public void onClick(View arg0) {
-                user.setfName(fName.getText().toString().trim());
-                user.setlName(lName.getText().toString().trim());
-                user.setEmail(mail.getText().toString().trim());
-                user.setPassword(passw.getText().toString().trim());
+                String firstName=fName.getText().toString().trim();
+                String lastName=lName.getText().toString().trim();
+                String Email=mail.getText().toString().trim();
+                String password=passw.getText().toString().trim();
+                String confirmPassword=confirm.getText().toString().trim();
 
-                reff.push().setValue(user);
-                ref.push().setValue(user);
-                Toast.makeText(context,"You have Signed up!",Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
+                if (TextUtils.isEmpty(firstName)){
+                    Toast.makeText(context,"Please enter your First Name to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(lastName)){
+                    Toast.makeText(context,"Please enter your Last Name to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }if (TextUtils.isEmpty(Email)){
+                    Toast.makeText(context,"Please enter your Email Address to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }if (TextUtils.isEmpty(password)){
+                    Toast.makeText(context,"Please enter your password to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }if (TextUtils.isEmpty(confirmPassword)){
+                    Toast.makeText(context,"Please confirm your password to continue.",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    Toast.makeText(context,"Please enter a valid Email address!",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (password.equals(confirmPassword)){
+                    firebaseAuth.createUserWithEmailAndPassword(Email, password)
+                            .addOnCompleteListener(activitySignup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(context,"Sign Up Complete!",Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(context, MainActivity.class);
+                                        startActivity(intent);
+
+                                    } else {
+                                        if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                                            Toast.makeText(context,"You are already registered.",Toast.LENGTH_LONG).show();
+                                            return;
+                                    }
+                                        Toast.makeText(context,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                                        return;
+
+                                    }
+
+                                    // ...
+                                }
+                            });
+
+                }
+                else {
+                    Toast.makeText(context,"The passwords you entered do no match!",Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+                //user.setfName(fName.getText().toString().trim());
+               // user.setlName(lName.getText().toString().trim());
+               // user.setEmail(mail.getText().toString().trim());
+               // user.setPassword(passw.getText().toString().trim());
+
+              //  reff.push().setValue(user);
+              //  ref.push().setValue(user);
+              //  Toast.makeText(context,"You have Signed up!",Toast.LENGTH_LONG).show();
+              //  Intent intent = new Intent(context, MainActivity.class);
+              //  startActivity(intent);
 
             }
 
