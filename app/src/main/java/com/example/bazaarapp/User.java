4bb06474a558636f175c5c3e6fd4bb1e2a550aea
FaiqaIftikhar <faiqaiftikharali@gmail.com>
2019-11-23 16:12:19 +0500

    faiqaSeven

---
+++ app/src/main/java/com/example/bazaarapp/User.java
@@ -0,0 +1,45 @@
+package com.example.bazaarapp;
+
+import android.provider.ContactsContract;
+
+public class User {
+    private String fName;
+    private String lName;
+    private String password;
+    private String Email;
+
+    public User() {
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
--- app/src/main/java/com/example/bazaarapp/activitySignup.java
+++ app/src/main/java/com/example/bazaarapp/activitySignup.java
@@ -7,14 +7,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class activitySignup extends AppCompatActivity {
-
+EditText fName,lName,mail,passw;
     Button button;
+    DatabaseReference reff;
+    DatabaseReference ref;
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        fName=findViewById(R.id.editTextfname);
+        lName=findViewById(R.id.editTextlname);
+        mail=findViewById(R.id.editTextemail);
+        passw=findViewById(R.id.editTextpassword);
+        user=new User();
+        reff= FirebaseDatabase.getInstance().getReference().child("User");
+
+        ref= FirebaseDatabase.getInstance().getReference().child("User");
+
         addListenerOnButton();
 
     }
@@ -32,7 +49,14 @@ public class activitySignup extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
+                user.setfName(fName.getText().toString().trim());
+                user.setlName(lName.getText().toString().trim());
+                user.setEmail(mail.getText().toString().trim());
+                user.setPassword(passw.getText().toString().trim());
 
+                reff.push().setValue(user);
+                ref.push().setValue(user);
+                Toast.makeText(context,"You have Signed up!",Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(context, MainActivity.class);
                 startActivity(intent);
 
